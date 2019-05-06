5a4032c4dbec82e6ed627bc5d00b5b3a02cdf5ec
simtaxx <souici.haris@gmail.com>
2019-05-06 11:05:12 +0200

    Components added

---
+++ app/components/Chill.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="big-header">
+    <h1>Page</h1>
+    <br>
+    <router-link class="button" to="/end">Lourd</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ app/components/Where.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="big-header">
+    <h1>Page</h1>
+    <br>
+    <router-link class="button" to="/">Chemin tranquille</router-link>
+    <router-link class="button" to="/">Chemin dangereux</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
