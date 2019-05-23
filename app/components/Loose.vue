c7feacfedb9fa5356b569030fb9cea512ca3b675
simtaxx <souici.haris@gmail.com>
2019-05-23 23:16:46 +0200

    lose

--- app/components/Loose.vue
+++ app/components/Loose.vue
@@ -1,8 +1,8 @@
-c<template>
+<template>
   <div class="screen">
     <h1>{{ message }}</h1>
     <br>
-    <router-link class="button" to="/">Recommencer</router-link>
+    <router-link class="buttonlose" to="/">Recommencer</router-link>
   </div>
 </template>
 
@@ -15,3 +15,22 @@ export default {
   }
 };
 </script>
+
+<style>
+.screen {
+  background-image: url("/assets/pictures/fondlose.png");
+  background-size: cover;
+}
+.buttonlose {
+  position: absolute;
+  bottom: 0;
+  left: 580px;
+  color: #083b77;
+  text-decoration: none;
+  background: #ea7739f3;
+  padding: 20px 35px 20px 35px;
+  border-radius: 5px;
+  font-size: 30px;
+  cursor: pointer;
+}
+</style>
