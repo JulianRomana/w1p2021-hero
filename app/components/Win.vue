f41cba0d18282faa96229b89e04991cf11a7b9d3
JulianRomana <jjulian.romana@gmail.com>
2019-05-24 12:55:26 +0200

    win component

--- app/components/Win.vue
+++ app/components/Win.vue
@@ -1,9 +1,9 @@
 c<template>
   <div class="big-header">
     <h1>{{ message }}</h1>
-    <br>
+    <h2>{{ thanks }}</h2>
     <router-link class="button" to="/">Recommencer</router-link>
-    <video src="/assets/videos/win.mp4" autoplay width="1440"></video>
+    <video src="/assets/videos/win.mp4" autoplay width="1440" />
   </div>
 </template>
 
@@ -11,16 +11,47 @@ c<template>
 export default {
   data() {
     return {
-      message: "Tu as retrouvé Sarutobi !"
+      message: 'Tu as retrouvé Sarutobi !',
+      thanks: 'Merci d\'avoir joué à notre Jeu ! '
     };
   }
 };
 </script>
-<style>
+<style scoped>
+.big-header {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 video {
   position: absolute;
   top: 0;
   left: 0;
+  z-index: 1;
+}
+h1 {
+  z-index: 2;
+
+  color: white;
+  text-transform: uppercase;
+  font-size: 40px;
+}
+h2 {
+  z-index: 2;
+  color: white;
+  text-transform: uppercase;
+  font-size: 25px;
+  margin-top: 20px;
+}
+.button {
+  z-index: 2;
+  bottom: 7%;
+  position: fixed;
+  margin-right: 0;
+  margin-left: 0;
 }
 </style>
 
